8ddcd03c707cc30cc2f25aaf283218573f6de335
Mankirat Singh <mankirat1998@gmail.com>
2017-11-24 20:20:41 +0530

    feat: dialog to choose file added

--- electron-music/main.js
+++ electron-music/main.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, dialog} = electron;
+const fs= require('fs');
 
 //Set env
 process.env.NODE_ENV = 'development';
@@ -36,16 +37,40 @@ app.on('ready', () => {
 
 function sendPauseSongMessage() {
   mainWindow.webContents.send('modal-pause-song', 'pause');
+  console.log("Pause");
 }
 
 function sendNextSongMessage() {
   mainWindow.webContents.send('modal-next-song', 'next');
+  console.log("Next");
 }
 
 function sendPrevSongMessage() {
   mainWindow.webContents.send('modal-prev-song', 'prev');
+  console.log("Previous");
+
 }
 
+// To select .mp3 file
 function openFileDialog() {
-
+  console.log("Open");
+  dialog.showOpenDialog(mainWindow, {properties: ['openDirectory']}, (filePath)=>{
+    if(filePath) {
+      fs.readdir(filePath[0], (err, files) => {
+        let arr = [];
+        for (let i = 0; i < files.length; i++) {
+          if (files[i].substr(-4) === '.mp3') {
+            arr.push(files[i]);
+          }
+        }
+        console.log(arr);
+        let objectToSend = {};
+        objectToSend.path = filePath[0];
+        objectToSend.files = arr;
+        mainWindow.webContents.send('modal-folder-content', objectToSend);
+      })
+    }else{
+      console.log("No file chosen");
+    }
+  })
 }
